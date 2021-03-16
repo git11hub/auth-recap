0110611dd0b64e0c757d8c185d693d416d998327
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-03-17 02:14:28 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCuSY47nyUwts87uf-P8x3EV9EBTm-Bw9s",
+    authDomain: "auth-recap-2fea6.firebaseapp.com",
+    projectId: "auth-recap-2fea6",
+    storageBucket: "auth-recap-2fea6.appspot.com",
+    messagingSenderId: "804861124037",
+    appId: "1:804861124037:web:0fb788c92419ae174fffac"
+  };
+
+  export default firebaseConfig;
